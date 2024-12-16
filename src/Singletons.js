d76947a84f52b18528229a78bbfbb28e73f5312e
AXCWG <xiey0@qq.com>
2024-12-16 20:06:40 +0800

    fixed

--- src/Singletons.js
+++ src/Singletons.js
@@ -1,11 +1,10 @@
 // TODO Always modify URL below before publishing.
 
-// const baseUrl = "https://andyxie.cn:8200/";
-const baseUrl = "http://localhost:8080/";
-// const fetchUrl = "https://andyxie.cn:8201/";
-const fetchUrl = "http://localhost:8079/";
-// const wsUrl = "wss://andyxie.cn:8202/";
-const wsUrl = "ws://localhost:8078/";
+const baseUrl = "https://andyxie.cn:8200/";
+// const baseUrl = "http://localhost:8080/";
+const fetchUrl = "https://andyxie.cn:8201/";
+// const fetchUrl = "http://localhost:8079/";
+
 
 const Kind = {
     0: "去和声伴奏",
