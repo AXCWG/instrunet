b449d0b22d3960c0477204af552ddd7d72c50ae7
axcwg <xiey0@qq.com>
2024-12-18 01:04:09 +0800

    https

--- src/Singletons.js
+++ src/Singletons.js
@@ -1,9 +1,9 @@
 // TODO Always modify URL below before publishing.
 
-// const baseUrl = "https://andyxie.cn:8200/";
-const baseUrl = "http://localhost:8080/";
-// const fetchUrl = "https://andyxie.cn:8201/";
-const fetchUrl = "http://localhost:8079/";
+const baseUrl = "https://andyxie.cn:8200/";
+// const baseUrl = "http://localhost:8080/";
+const fetchUrl = "https://andyxie.cn:8201/";
+// const fetchUrl = "http://localhost:8079/";
 
 
 const Kind = {
